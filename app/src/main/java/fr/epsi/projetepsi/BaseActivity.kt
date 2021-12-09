78fed6f806a3c6aefc996cc02e151ce727bb94e9
LoicLabaisse <labaisse.loic@gmail.com>
2021-12-09 15:50:23 +0100

    add BaseActivity and SplashActivity , starting the menu on main

--- app/src/main/java/fr/epsi/projetepsi/BaseActivity.kt
+++ app/src/main/java/fr/epsi/projetepsi/BaseActivity.kt
@@ -2,10 +2,22 @@ package fr.epsi.projetepsi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
 
-class BaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+open class BaseActivity : AppCompatActivity() {
+    fun showBtnBack(){
+        val imageViewBack=findViewById<ImageView>(R.id.imageViewBack)
+        imageViewBack.visibility=View.VISIBLE
+        imageViewBack.setOnClickListener(View.OnClickListener {
+            finish()
+        })
+    }
+
+    fun setHeaderTitle(txt : String){
+        val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.text = txt
     }
 }
